9e994173323cd488c2333a2669c691a8c17ca761
ryseok <40780816+ryseok@users.noreply.github.com>
2018-09-20 20:02:25 +0900

    no message

--- src/main/java/com/localup/test/EmailDAO.java
+++
@@ -1,9 +0,0 @@
-package com.localup.test;
-
-public interface EmailDAO {
-	//생성
-	public void create(EmailForm emailForm) throws Exception;
-	
-	//업데이트
-	public void update(EmailForm emailForm) throws Exception;
-}
--- src/main/java/com/localup/test/EmailService.java
+++
@@ -1,7 +0,0 @@
-package com.localup.test;
-
-public interface EmailService {
-	public void create(EmailForm emailForm) throws Exception;
-	
-	public void update(EmailForm emailForm) throws Exception;
-}

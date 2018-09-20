1850275d7fe50cade6f29b2d0acb3d8646d1a85f
ryseok <40780816+ryseok@users.noreply.github.com>
2018-09-20 19:47:00 +0900

    no message

--- src/main/java/com/localup/test/UserDAO.java
+++
@@ -1,5 +0,0 @@
-package com.localup.test;
-
-public interface UserDAO {
-	public Object login(UserVO uservo) throws Exception;
-}
--- src/main/java/com/test/myapp/HomeController.java
+++ src/main/java/com/test/myapp/HomeController.java
@@ -1,4 +1,4 @@
-package com.localup.control;
+package com.test.myapp;
 
 import java.text.DateFormat;
 import java.util.Date;

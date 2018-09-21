c665951ada70b88acba1c834c596c98ad5bc8fa1
DESKTOP-RD6FESM\Playdata <ognan@naver.com>
2018-09-21 11:27:18 +0900

    no message

---
+++ src/main/java/com/localup/control/TestController.java
@@ -0,0 +1,13 @@
+package com.localup.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping("test")
+	public void test() {
+		System.out.println("테스트페이지 요청...");
+	}
+}

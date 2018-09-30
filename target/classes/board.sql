c14c74637f80a14e439e8269f135dc42f12694b3
DESKTOP-L1TG8NV\user <ssh32009757@gmail.com>
2018-09-30 12:06:34 +0900

    20180930 12:06 DB 수정 및 인스턴스 변경

--- target/classes/board.sql
+++ target/classes/board.sql
@@ -2,7 +2,7 @@
 drop table board;
 CREATE TABLE board (
 	board_no NUMBER NOT NULL, /* 게시글번호 */
-	member_id VARCHAR2(15) NOT NULL, /* 회원ID */
+	member_email VARCHAR2(100) NOT NULL, /* 회원ID */
 	board_type VARCHAR2(20) NOT NULL, /* 게시글유형 */
 	board_title VARCHAR2(100) NOT NULL, /* 게시글제목 */
 	board_cont VARCHAR2(1000), /* 게시글내용 */
@@ -31,10 +31,10 @@ ALTER TABLE board
 	ADD
 		CONSTRAINT FK_member_TO_board
 		FOREIGN KEY (
-			member_id
+			member_email
 		)
 		REFERENCES member (
-			member_id
+			member_email
 		);
 drop sequence board_seq;
 create sequence board_seq
@@ -43,15 +43,15 @@ increment by 1
 nocache
 nocycle;		
 
-insert into BOARD(board_no, member_id, board_type, board_title, board_cont, board_img, board_view, board_like, board_alti, board_long, board_max) 
-values(board_seq.nextval, 'gildong', '맛집','어디까지 가봤니?','난 여기까지 가봤단다1','mathouse1.jpg',13,8,37.1,126.1,5); 
-insert into BOARD(board_no, member_id, board_type, board_title, board_cont, board_img, board_view, board_like, board_alti, board_long, board_max) 
-values(board_seq.nextval, 'gildong', '맛집','어디까지 가봤니?','난 여기까지 가봤단다2','mathouse2.jpg',13,8,37.2,126.2,5); 
-insert into BOARD(board_no, member_id, board_type, board_title, board_cont, board_img, board_view, board_like, board_alti, board_long, board_max) 
-values(board_seq.nextval, 'gildong', '맛집','어디까지 가봤니?','난 여기까지 가봤단다3','mathouse3.jpg',13,8,37.3,126.3,5); 
-insert into BOARD(board_no, member_id, board_type, board_title, board_cont, board_img, board_view, board_like, board_alti, board_long, board_max) 
-values(board_seq.nextval, 'gildong', '맛집','어디까지 가봤니?','난 여기까지 가봤단다4','mathouse4.jpg',13,8,37.4,126.4,5); 
-insert into BOARD(board_no, member_id, board_type, board_title, board_cont, board_img, board_view, board_like, board_alti, board_long, board_max) 
-values(board_seq.nextval, 'gildong', '맛집','어디까지 가봤니?','난 여기까지 가봤단다5','mathouse5.jpg',13,8,37.5,126.5,5); 
-insert into BOARD(board_no, member_id, board_type, board_title, board_cont, board_img, board_view, board_like, board_alti, board_long, board_max) 
-values(board_seq.nextval, 'gildong', '맛집','어디까지 가봤니?','난 여기까지 가봤단다6','mathouse6.jpg',13,8,37.6,126.6,5); 
+insert into BOARD(board_no, member_email, board_type, board_title, board_cont, board_img, board_view, board_like, board_alti, board_long, board_max) 
+values(board_seq.nextval, 'gildong@naver.com', '맛집','어디까지 가봤니?','난 여기까지 가봤단다1','mathouse1.jpg',13,8,37.1,126.1,5); 
+insert into BOARD(board_no, member_email, board_type, board_title, board_cont, board_img, board_view, board_like, board_alti, board_long, board_max) 
+values(board_seq.nextval, 'gildong@naver.com', '맛집','어디까지 가봤니?','난 여기까지 가봤단다2','mathouse2.jpg',13,8,37.2,126.2,5); 
+insert into BOARD(board_no, member_email, board_type, board_title, board_cont, board_img, board_view, board_like, board_alti, board_long, board_max) 
+values(board_seq.nextval, 'gildong@naver.com', '맛집','어디까지 가봤니?','난 여기까지 가봤단다3','mathouse3.jpg',13,8,37.3,126.3,5); 
+insert into BOARD(board_no, member_email, board_type, board_title, board_cont, board_img, board_view, board_like, board_alti, board_long, board_max) 
+values(board_seq.nextval, 'gildong@naver.com', '맛집','어디까지 가봤니?','난 여기까지 가봤단다4','mathouse4.jpg',13,8,37.4,126.4,5); 
+insert into BOARD(board_no, member_email, board_type, board_title, board_cont, board_img, board_view, board_like, board_alti, board_long, board_max) 
+values(board_seq.nextval, 'gildong@naver.com', '맛집','어디까지 가봤니?','난 여기까지 가봤단다5','mathouse5.jpg',13,8,37.5,126.5,5); 
+insert into BOARD(board_no, member_email, board_type, board_title, board_cont, board_img, board_view, board_like, board_alti, board_long, board_max) 
+values(board_seq.nextval, 'gildong@naver.com', '맛집','어디까지 가봤니?','난 여기까지 가봤단다6','mathouse6.jpg',13,8,37.6,126.6,5); 

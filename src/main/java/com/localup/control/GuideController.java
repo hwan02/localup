ab1cfb9e4520c2815b1b1dff98c98bede9a0aa46
rys <ryseok@gmail.com>
2018-10-02 01:02:33 +0900

    181002 : pom.xml ===> 추가   <!--imageUpload -->   <!-- https://mvnrepository.com/artifact/org.imgscalr/imgscalr-lib -->   <dependency>    <groupId>org.imgscalr</groupId>    <artifactId>imgscalr-lib</artifactId>    <version>4.2</version>   </dependency>

--- src/main/java/com/localup/control/GuideController.java
+++ src/main/java/com/localup/control/GuideController.java
@@ -24,48 +24,48 @@ import com.localup.persistence.GuideDAOImpl;
 import com.localup.service.GuideService;
 
 @Controller
-//@RequestMapping("guide")
+@RequestMapping("guide")
 public class GuideController {
 	
-//	//이미지 TEST
-//	private static final Logger logger = LoggerFactory.getLogger(GuideController.class);
-//	
-//	@Inject
-//	GuideService guideService;
-//	
-//	//이미지 DB 입력
-//	@Resource(name="uploadPath")
-//	private String uploadPath;
-//	
-//	//가이드 상세 페이지 (작성자 : rys)
-//	//가이드 상세 페이지 폼 보기
-//	@RequestMapping("guideDetailPage")
-//	public String guideDetailPage() throws Exception {
-//		return "board/guide";
-//	}
-//	
-//	//가이드 상세 페이지 등록 폼보기
-////	@RequestMapping(value="guideWrite",method=RequestMethod.GET)
-////	public String guideWriteGET() throws Exception {
-////		return "board/guideWrite";
-////	}
-//
-//	//이미지 TEST
+	//이미지 TEST
+	//private static final Logger logger = LoggerFactory.getLogger(GuideController.class);
+	
+	@Inject
+	GuideService guideService;
+	
+	//이미지 DB 입력
+	@Resource(name="uploadPath")
+	private String uploadPath;
+	
+	//가이드 상세 페이지 (작성자 : rys)
+	//가이드 상세 페이지 폼 보기
+	@RequestMapping("guideDetailPage")
+	public String guideDetailPage() throws Exception {
+		return "board/guide";
+	}
+	
+	//가이드 상세 페이지 등록 폼보기
+	@RequestMapping(value="guideWrite",method=RequestMethod.GET)
+	public String guideWriteGET() throws Exception {
+		return "board/guideWrite";
+	}
+
+	//이미지 TEST
 //	@RequestMapping(value="guideWrite",method=RequestMethod.GET)
 //	public String guideWriteGET() throws Exception {
 //		return "board/guideWriteTest";
 //	}
-//	
-//	//가이드 상세 페이지 등록 ===> DB 입력처리
-////	@RequestMapping(value="guideWrite",method=RequestMethod.POST)
-////	public String guideWritePOST(GuideVO guideVO, MultipartFile tour_img) throws Exception {
-////		String savedName = uploadFile(tour_img.getOriginalFilename(),tour_img.getBytes());
-////		//guideVO.setTour_img(savedName);
-////		//guideService.insert(guideVO);
-////		return "main/main";
-////	}
-//	
-//	//이미지 TEST
+	
+	//가이드 상세 페이지 등록 ===> DB 입력처리
+	@RequestMapping(value="guideWrite",method=RequestMethod.POST)
+	public String guideWritePOST(GuideVO guideVO, MultipartFile tour_img) throws Exception {
+		String savedName = uploadFile(tour_img.getOriginalFilename(),tour_img.getBytes());
+		//guideVO.setTour_img(savedName);
+		//guideService.insert(guideVO);
+		return "main/main";
+	}
+	
+	//이미지 TEST
 //	@RequestMapping(value="guideWrite",method=RequestMethod.POST)
 //	public String guideWritePOST(GuideVO guideVO,MultipartFile tour_img) throws Exception {
 //		String savedName = uploadFile(tour_img.getOriginalFilename(),tour_img.getBytes());
@@ -73,50 +73,20 @@ public class GuideController {
 //		guideService.insert(guideVO);
 //		return "redirect:result";
 //	}
-//	
+	
 //	//이미지 TEST
 //	@RequestMapping("result")
 //	public String result() {
 //		return "board/guideWriteTest2";
 //	}
-//	
-//	
-//	private String uploadFile(String originalName, byte[] fileData) throws Exception {
-//		UUID uid = UUID.randomUUID();
-//		String savedName = uid.toString() + "_" + originalName;
-//		File target = new File(uploadPath,savedName);
-//		FileCopyUtils.copy(fileData, target);
-//		return savedName;
-//	}
 	
 	
-//============================================이미지 TEST 시작
-	@Inject
-	private GuideDAOImpl dao;
-	     
-	/**
-	 * 파일태그를 위한 폼태그
-	 * @return
-	 */
-	@RequestMapping(value="/formFile")
-	public String formFile() {
-	    return "board/guideWriteTest";
+	private String uploadFile(String originalName, byte[] fileData) throws Exception {
+		UUID uid = UUID.randomUUID();
+		String savedName = uid.toString() + "_upload_" + originalName;
+		File target = new File(uploadPath,savedName);
+		FileCopyUtils.copy(fileData, target);
+		return savedName;
 	}
-	 
-	/**
-	 * 파일처리 컨트롤러
-	 * @param vo
-	 * @return
-	 */
-	@RequestMapping(value="/saveImage")
-	public String saveImage(GuideVO vo) {
-	    try {
-	        Map<String, Object> hmap = new HashMap<String, Object>();
-	        hmap.put("img", vo.getImgFile().getBytes());
-	        dao.saveImage(hmap);   
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }
-	    return "redirect:/formFile";
-	}
+
 }

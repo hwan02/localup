74ac4bbc52a05f272dfdafc4c41e296bf90f4c78
DESKTOP-3TARHPM\Playdata <fls072@naver.com>
2018-10-04 21:09:23 +0900

    댓글 arc에서 db에 들어가짐

--- src/main/java/com/localup/persistence/ReplyDAO.java
+++ src/main/java/com/localup/persistence/ReplyDAO.java
@@ -2,6 +2,7 @@ package com.localup.persistence;
 
 import java.util.List;
 
+import com.localup.domain.Criteria;
 import com.localup.domain.ReplyVO;
 
 public interface ReplyDAO {
@@ -10,4 +11,14 @@ public interface ReplyDAO {
 	
 	public List<ReplyVO> selectReply(int board_no) throws Exception;
 	
+	public void updateReply(ReplyVO replyVO) throws Exception;
+	
+	public void deleteReply(int reply_no) throws Exception;
+	
+	public List<ReplyVO> listPage(int board_no, Criteria cri) throws Exception;
+	public int count(int board_no) throws Exception;
+	
+	//게시물 번호 얻기
+	public int getBno(int reply_no) throws Exception;
+
 }
